a6f388faf55daf2859edcbaf934c05722511e323
Valentin Lepmets <mosithean@gmail.com>
2022-02-03 07:36:11 +0100

    Handle errors

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -10,6 +10,8 @@ namespace API.DTOs
     {
         [Required]
         public string Username { get; set; }
+
+        [StringLength(8, MinimumLength = 4)]
         [Required]
         public string Password { get; set; }
     }
--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -6,6 +6,9 @@ import { HomeComponent } from './home/home.component';
 import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MemberListComponent } from './members/member-list/member-list.component';
+import { TestErrorsComponent } from './errors/test-errors/test-errors.component';
+import { NotFoundComponent } from './errors/not-found/not-found.component';
+import { ServerErrorComponent } from './errors/server-error/server-error.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
@@ -22,7 +25,10 @@ const routes: Routes = [
       { path: 'members/:id', component: MemberDetailComponent },
       { path: 'lists', component: ListsComponent },
       { path: 'messages', component: MessagesComponent },
-      { path: '**', component: HomeComponent, pathMatch: 'full' },
+      { path: 'errors', component: TestErrorsComponent },
+      { path: 'not-found', component: NotFoundComponent },
+      { path: 'server-error', component: ServerErrorComponent },
+      { path: '**', component: NotFoundComponent, pathMatch: 'full' },
     ],
   },
 ];
